f76411daccca305789b68fc6995e0f53c7a50b09
Husnain Ali <husnain.ali@arhamsoft.com>
2020-06-18 16:09:21 +0500

    updated code
    updated code

--- run.R
+++ run.R
@@ -1,74 +1,146 @@
-library(fiery)
-library(routr)
+library(shiny)
+library(rvest)
+library(stats)
+library(jsonlite)
+library(dplyr, warn.conflicts = FALSE)
+library(shinycssloaders)
 
-set.seed(1492)
-x <- rnorm(15)
-y <- x + rnorm(15)
-fit <- lm(y ~ x)
-saveRDS(fit, "model.rds")
 
-app <- Fire$new(host = '0.0.0.0', port = as.integer(Sys.getenv('PORT')))
-app$set_logger(logger_console())
+# UI Part
 
-# When the app starts, we'll load the model we saved. Instead of
-# polluting our namespace we'll use the internal data store
-
-app$on('start', function(server, ...) {
-  server$set_data('model', readRDS('model.rds'))
-  message('Model loaded')
-})
-
-# Just for show off, we'll make it so that the model is automatically
-# passed on to the request handlers
-
-app$on('before-request', function(server, ...) {
-  list(model = server$get_data('model'))
-})
-
-# Now comes the biggest deviation. We'll use routr to define our request
-# logic, as this is much nicer
-router <- RouteStack$new()
-route <- Route$new()
-router$add_route(route, 'main')
-
-# We start with a catch-all route that provides a welcoming html page
-route$add_handler('get', '*', function(request, response, keys, ...) {
-  response$type <- 'html'
-  response$status <- 200L
-  response$body <- '<h1>All your AI are belong to us</h1>'
-  TRUE
-})
-# Then on to the /info route
-route$add_handler('get', '/info', function(request, response, keys, ...) {
-  response$status <- 200L
-  response$body <- structure(R.Version(), class = 'list')
-  response$format(json = reqres::format_json())
-  TRUE
-})
-# Lastly we add the /predict route
-route$add_handler('get', '/predict', function(request, response, keys, arg_list, ...) {
-  response$body <- predict(
-    arg_list$model, 
-    data.frame(x=as.numeric(request$query$val)),
-    se.fit = TRUE
+##
+ui <- fluidPage(
+  titlePanel("Web Scrapping"),
+  sidebarLayout(
+    sidebarPanel(
+      textInput("url", "Enter URL", value = '', width = NULL, placeholder = "URL of website to read data..."),
+      textInput("css", "Enter Css", value = '', width = NULL, placeholder = "Css of website to read data..."),
+      radioButtons("productInput", "What Kind of product data you want?",
+                   choices = c("Name", "Price", "Name and price", "Complete Detail"),
+                   selected = "Complete Detail"),
+      
+    ),
+    mainPanel(
+      h3("Parsed query string"),
+      verbatimTextOutput("queryText"),
+      br(),br(),br(),
+      tableOutput("results") %>% withSpinner()
+    )
   )
-  response$status <- 200L
-  response$format(json = reqres::format_json())
-  TRUE
-})
-# And just to show off reqres file handling, we'll add a route 
-# for getting a model plot
-route$add_handler('get', '/plot', function(request, response, keys, arg_list, ...) {
-  f_path <- tempfile(fileext = '.png')
-  png(f_path)
-  plot(arg_list$model)
-  dev.off()
-  response$status <- 200L
-  response$attach(f_path, filename = 'model_plot.png')
-  TRUE
-})
+)
 
-# Finally we attach the router to the fiery server
-app$attach(router)
+# Server Part
+server = function(input, output, session) {
+  
+  output$queryText <- renderText({
+    query <- parseQueryString(session$clientData$url_search)
+    
+    # Return a string with key-value pairs
+    paste(names(query), query, sep = "=", collapse=", ")
+  })
+  
+  result <- reactive({
+    if (input$url == "") {
+      return()
+    }
+    
+    if (input$css == "") {
+      if (input$productInput == "") {
+        return()
+      }else{
+        if (input$productInput == "Name") {
+          Css <- c(".BiggerText a")
+        }else if (input$productInput == "Price") {
+          Css <- c(".PriceFont")
+        }else if (input$productInput == "Name and price") {
+          Css <- c(".BiggerText a", ".PriceFont")
+        }else if (input$productInput == "Complete Detail") {
+          Css <- c(".BiggerText a", ".PriceFont")
+        }
+      }
+    }else{
+      Css <- c(input$css)
+    }
+    
+    URL <- input$url
+    res <- StartScraping(URL, Css)
+  })
+  
+  output$results <- renderTable({
+    if (is.null(result())) {
+      return()
+    }
+    
+    result()
+  })
+}
 
-app$ignite()
+StartScraping  <- function(url = '', css = c()) {
+  # Start Scraping
+  print(url)
+  print(css)
+  trails_webpage <- read_html(url)
+  
+  if (length(css) == 1) {
+    print("1")
+    
+    ouputName <- trails_webpage %>% html_nodes(css[1]) %>% html_text()
+    
+    index = NULL
+    for (value in 1:length(ouputName)) {
+      if(ouputName[value] == "\n") {
+        index = append(index, value)
+      }
+    }
+    
+    Name <- ouputName[ouputName != "\n"]
+    datFrame <- data.frame(Name)
+    
+  }else if (length(css) == 2) {
+    
+    print("2")
+    trail_names_html <- html_nodes(trails_webpage, css[1])
+    outputName <- html_text(trail_names_html)
+    
+    trail_names_html <- html_nodes(trails_webpage, css[2])
+    outputPrice <- html_text(trail_names_html)
+    
+    Prices = NULL
+    Names = NULL
+    for (value in 1:length(outputName)) {
+      if(outputName[value] != "\n") {
+        Prices <- append(Prices, outputPrice[value])
+        Names <- append(Names, outputName[value])
+      }
+    }
+    
+    datFrame <- data.frame(Names, Prices)
+  }else if (length(css) == 3) {
+    print("3")
+    trail_names_html <- html_nodes(trails_webpage, css[1])
+    outputName <- html_text(trail_names_html)
+    
+    trail_names_html <- html_nodes(trails_webpage, css[2])
+    outputPrice <- html_text(trail_names_html)
+    
+    outputLinks <- paste0(url, trails_webpage %>%  html_nodes( paste(css[3], "a", sep = " ")) %>%  html_attr("href"))
+    
+    Prices = NULL
+    Names = NULL
+    URLs = NULL
+    
+    for (value in 1:length(outputName)) {
+      print(outputName[value])
+      if(outputName[value] != "\n") {
+        Prices <- append(Prices, outputPrice[value])
+        Names <- append(Names, outputName[value])
+        URLs <- append(URLs, outputLinks[value])
+      }
+    }
+    
+    datFrame <- data.frame(Names, Prices, URLs)
+  }
+}
+
+# Start An App
+shinyApp(ui = ui, server = server)
